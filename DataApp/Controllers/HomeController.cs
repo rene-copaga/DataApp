3ba20ecb8659af18e447bb807db6148ec863000e
Rene Copaga <rene.copaga@gmail.com>
2020-05-04 16:11:44 -0400

    Creating the ASP.NET Core MVC Project

---
+++ DataApp/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using DataApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DataApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new Product[] {
+                new Product { Name = "P1", Category = "Cat1", Price = 10 },
+                new Product { Name = "P2", Category = "Cat2", Price = 20 },
+                new Product { Name = "P3", Category = "Cat3", Price = 30 },
+            });
+        }
+    }
+}
---
+++ DataApp/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace DataApp.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+    }
+}
