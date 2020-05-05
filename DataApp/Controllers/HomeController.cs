a19c3784cb8cff68662dc9a522553aaeee5b4fdf
Rene Copaga <rene.copaga@gmail.com>
2020-05-04 20:27:28 -0400

    Adding and Configuring Entity Framework Core

--- DataApp/Controllers/HomeController.cs
+++ DataApp/Controllers/HomeController.cs
@@ -5,13 +5,16 @@ namespace DataApp.Controllers
 {
     public class HomeController : Controller
     {
+        private EFDatabaseContext context;
+
+        public HomeController(EFDatabaseContext ctx)
+        {
+            context = ctx;
+        }
+
         public IActionResult Index()
         {
-            return View(new Product[] {
-                new Product { Name = "P1", Category = "Cat1", Price = 10 },
-                new Product { Name = "P2", Category = "Cat2", Price = 20 },
-                new Product { Name = "P3", Category = "Cat3", Price = 30 },
-            });
+            return View(context.Products);
         }
     }
 }
---
+++ DataApp/Models/EFDatabaseContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataApp.Models
+{
+    public class EFDatabaseContext : DbContext
+    {
+        public EFDatabaseContext(DbContextOptions<EFDatabaseContext> opts)
+            : base(opts) {}
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
